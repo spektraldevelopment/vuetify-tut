d52dba0428b972e974da4f20737e1f4eeb09d94a
Dave Boyle <foo@bar.com>
2019-11-23 17:46:51 -0500

    [master] - Lesson 4

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p class="red white--text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure voluptatem, fugiat unde tenetur quasi, eum doloribus quas, reprehenderit sint porro consectetur! Tempore eaque possimus, obcaecati laboriosam ducimus dolores corporis sapiente.</p>
+    <p class="pink lighten-4 red--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste cumque incidunt recusandae molestiae officiis quaerat, aspernatur ducimus delectus quidem deserunt sint cum velit debitis expedita culpa sapiente vitae enim asperiores.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Small Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a sub heading</p>
+    <p class="caption">This is a caption</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home'
+}
+</script>
