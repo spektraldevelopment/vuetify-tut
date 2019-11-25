725e7e1d9c5848570cf74d605156e78c480dd2c7
Dave Boyle <foo@bar.com>
2019-11-25 13:15:03 -0500

    [lesson-10] - Themes and Lists.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,10 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        primary: '#9652ff',
+        success: '#3cd1c2',
+        info: '#ffaa2c',
+        error: '#f83e70'
+    }
 });
