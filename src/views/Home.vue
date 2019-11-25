da7522b739a8037c24ea45ddd710fe27ae076ef6
Dave Boyle <foo@bar.com>
2019-11-25 11:09:20 -0500

    [lesson-6] - Breakpoints

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
+
+    <v-btn class="hidden-md-and-down">Hidden md and down</v-btn>
+
+    <v-btn class="hidden-md-and-up">Hidden md and up</v-btn>
+    
+    <v-btn class="hidden-sm-only">Small only</v-btn>
+
   </div>
 </template>
 
