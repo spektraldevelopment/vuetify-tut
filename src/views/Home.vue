1d637ed5e2aa1c1f6b1cf051d8cf06ec5ca9115c
Dave Boyle <foo@bar.com>
2019-11-25 10:43:59 -0500

    [lesson-5] - Buttons and icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
+
+    <v-btn class="pink white--text">Click me</v-btn>
+    <v-btn depressed color="blue">Click me</v-btn>
+    <v-btn text color="pink">Click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>Email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>Email me</span>
+    </v-btn>
+
+     <v-btn depressed large class="pink white--text">
+      <span>Email me</span>
+      <v-icon right large>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+
+
   </div>
 </template>
 
